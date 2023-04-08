911d0168fb764eea74ffcf2a92d10e7d3de15479
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-04-08 19:45:15 +0200

    feat(env-scaffolder): add description to arguments/options

--- tools/environment-scaffolder/src/index.ts
+++ tools/environment-scaffolder/src/index.ts
@@ -11,33 +11,54 @@ await yargs(hideBin(process.argv))
         .option('repository', {
           alias: 'repo',
           type: 'string',
+          description:
+            'The repository to create the environment in. Format: <owner>/<name>',
         })
         .option('environment', {
           alias: 'env',
           type: 'string',
+          description: 'The name of the environment to create',
         })
         .option('namespacePrefix', {
           alias: 'nsPrefix',
           type: 'string',
+          description: 'The prefix to use for the namespace',
         })
         .option('clusterIssuer', {
           type: 'string',
+          description: 'The cluster issuer to use for certificates',
         })
         .option('hostname', {
           type: 'string',
+          description: 'The hostname to use for the environment',
         })
         .option('delimiter', {
           type: 'string',
+          description: '(optional) The delimiter to use for the namespace',
         });
     },
-    async ({ repository, environment, namespacePrefix, clusterIssuer, hostname, delimiter = '-' }) => {
+    async ({
+      repository,
+      environment,
+      namespacePrefix,
+      clusterIssuer,
+      hostname,
+      delimiter = '-',
+    }) => {
       if (!repository || !repository.includes('/')) throw new Error();
       if (!environment) throw new Error();
       if (!namespacePrefix) throw new Error();
       if (!clusterIssuer) throw new Error();
       if (!hostname) throw new Error();
 
-      await createEnvironment(repository, environment, namespacePrefix, clusterIssuer, hostname, delimiter);
+      await createEnvironment(
+        repository,
+        environment,
+        namespacePrefix,
+        clusterIssuer,
+        hostname,
+        delimiter
+      );
     }
   )
   .parse();
